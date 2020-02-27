b405b860e2476902a9fec41f22efa92555be5dd2
Luis Portugal <luisenriquegomesportugal@gmail.com>
2020-02-27 18:22:44 -0300

    refactor: reset password

--- backend/app/Controllers/Http/ForgotPasswordController.js
+++ backend/app/Controllers/Http/ForgotPasswordController.js
@@ -1,29 +1,67 @@
 "use strict";
 
+const moment = require("moment");
 const crypto = require("crypto");
 const Mail = use("Mail");
 const User = use("App/Models/User");
 
 class ForgotPasswordController {
-  async store({ request }) {
-    const { email, redirect_to } = request.all();
-    const user = await User.findBy("email", email);
+  async store({ request, response }) {
+    try {
+      const { email, redirect_to } = request.all();
+      const user = await User.findByOrFail("email", email);
 
-    user.token = crypto.randomBytes(10).toString("hex");
-    user.token_created_at = new Date();
+      user.token = crypto.randomBytes(10).toString("hex");
+      user.token_created_at = new Date();
 
-    await user.save();
+      await user.save();
 
-    await Mail.send(
-      ["emails.forgot_password", "emails.forgot_password-text"],
-      { email, token: user.token, link: `${redirect_to}?token=${user.token}` },
-      message => {
-        message
-          .to(user.email)
-          .from("luisenriquegomesportugal@gmail.com", "Luis Portugal")
-          .subject("Recuperação de Senha");
+      await Mail.send(
+        ["emails.forgot_password", "emails.forgot_password-text"],
+        {
+          email,
+          token: user.token,
+          link: `${redirect_to}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from("luisenriquegomesportugal@gmail.com", "Luis Portugal")
+            .subject("Recuperação de Senha");
+        }
+      );
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: "Algo não deu certo, esse e-mail existe?" }
+      });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+      const user = await User.findByOrFail("token", token);
+
+      const tokenExpired = moment()
+        .subtract("2", "days")
+        .isAfter(user.token_created_at);
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: "O token de recuperação está expirado!" }
+        });
       }
-    );
+
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+
+      await user.save();
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: "Algo deu errado ao resetar sua senha" }
+      });
+    }
   }
 }
 
